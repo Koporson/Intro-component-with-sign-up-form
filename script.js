d5fb21981ccd5d0c9d097af018e21e7407649d14
Koporson <108631492+Koporson@users.noreply.github.com>
2022-07-12 23:34:21 +0200

    changes

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-const inputField = document.querySelectorAll('.input');
-const btn = document.querySelector('.button');
+const form = document.querySelector("form");
+const inputFirst = form.querySelector(".firstName");
+const inputSecond = form.querySelector(".lastName");
+const inputEmail = form.querySelector(".mail");
+const inputPassword = form.querySelector(".pass");
+const btn = document.querySelector(".button");
+const firstError = form.querySelector(".fError");
+const lastError = form.querySelector(".lError");
+const emailError = form.querySelector(".eError");
+const passwordError = form.querySelector(".pError");
 
-for(let i = 0; i < inputField.length; i++){
-    
+form.addEventListener("submit", checkInputs);
+
+function checkInputs(e) {
+	let validRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+	if (inputFirst.value === "" || inputFirst.value == null) {
+		inputFirst.classList.add("validinput");
+		firstError.innerText = "First Name cannot be empty";
+		e.preventDefault();
+	} else if (inputFirst.value === inputFirst.value) {
+		inputFirst.classList.remove("validinput");
+		firstError.innerText = "";
+	}
+	if (inputSecond.value === "" || inputSecond.value == null) {
+		inputSecond.classList.add("validinput");
+		lastError.innerText = "Last Name cannot be empty";
+		e.preventDefault();
+	} else if (inputSecond.value === inputSecond.value) {
+		inputSecond.classList.remove("validinput");
+		lastError.innerText = "";
+	}
+	if (inputEmail.value === "" || inputEmail.value == null) {
+		inputEmail.classList.add("validinput");
+		emailError.innerText = "Email cannot be empty";
+		e.preventDefault();
+	}
+	if (inputPassword.value === "" || inputPassword.value == null) {
+		inputPassword.classList.add("validinput");
+		passwordError.innerText = "Password cannot be empty";
+	} else if (inputPassword.value === inputPassword.value) {
+		inputPassword.classList.remove("validinput");
+		passwordError.innerText = "";
+	}
+	//valid regex
+	if (inputEmail.value.match(validRegex)) {
+		alert("eo");
+		return true;
+	} else {
+		inputEmail.placeholder = "email@example/com";
+		inputEmail.classList.add("validinput");
+		emailError.innerText = "Looks like this is not email";
+		console.log("chuj");
+		return false;
+	}
 }
